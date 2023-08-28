53b293555144ffb59947a03992d4e742f1074ab4
braine <michael.braine@nist.gov>
2023-08-28 10:17:34 -0400

    updated version.py for release

--- src/var/version.py
+++ src/var/version.py
@@ -1,8 +1,11 @@
 version = "v1.03"
-versionDate = "23 August, 2023"
+versionDate = "28 August, 2023"
 author = "Michael Braine"
 changes = [
     "- residuals in chart now visualized as \% of irradiance",
     "- added version number to output csv",
-    "- "
+    "- added residuals to output csv",
+    "- fixed version number disappearing in status bar, now appears on right side",
+    "- added icon to main window",
+    "- added splash screen when using exe",
 ]
