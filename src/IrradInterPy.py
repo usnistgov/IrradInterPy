e643a80ffb4353d9141ffbc877d2523fe3a352bf
braine <michael.braine@nist.gov>
2023-08-28 10:16:29 -0400

    Force splash screen to close when exe finishes unpacking

--- src/IrradInterPy.py
+++ src/IrradInterPy.py
@@ -2,9 +2,16 @@ import sys
 from PyQt5 import QtWidgets
 from GUI.IrradianceInterpolationMainGUI import Window_IrradianceInterpolationMain
 
+if getattr(sys, "frozen", False):
+    import pyi_splash
+
 print("\nLaunching Irradiance Interpolation ...\n")
 IrradianceInterpolationApp = QtWidgets.QApplication(sys.argv)
 IrradianceInterpolationWindow = Window_IrradianceInterpolationMain()
+
+if getattr(sys, "frozen", False):
+    pyi_splash.close()
+
 IrradianceInterpolationWindow.show()
 IrradianceInterpolationApp.exec()
 
