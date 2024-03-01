cd840b66b209ca3f99b5ef31df59abfa35807415
braine <michael.braine@nist.gov>
2024-03-01 13:02:11 -0500

    Updated version

--- src/var/version.py
+++ src/var/version.py
@@ -1,11 +1,6 @@
-version = "v1.03"
-versionDate = "28 August, 2023"
+version = "v1.04"
+versionDate = "1 March, 2024"
 author = "Michael Braine"
 changes = [
-    "- residuals in chart now visualized as \% of irradiance",
-    "- added version number to output csv",
-    "- added residuals to output csv",
-    "- fixed version number disappearing in status bar, now appears on right side",
-    "- added icon to main window",
-    "- added splash screen when using exe",
+    "- fixed issue when importing data that is sorted by decreasing wavelength",
 ]
