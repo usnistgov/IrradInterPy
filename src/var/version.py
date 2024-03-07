3d1179737d4f67a47f51d976465556c6c75ecc83
braine <michael.braine@nist.gov>
2024-03-07 16:15:07 -0500

    Updated version

--- src/var/version.py
+++ src/var/version.py
@@ -1,6 +1,6 @@
-version = "v1.04"
-versionDate = "1 March, 2024"
+version = "v1.05"
+versionDate = "7 March, 2024"
 author = "Michael Braine"
 changes = [
-    "- fixed issue when importing data that is sorted by decreasing wavelength",
+    "- added dof=2 as option",
 ]
