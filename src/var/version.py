4b68fc539aa64aa8980e168618abcb4af6532214
braine <michael.braine@nist.gov>
2024-03-08 08:55:27 -0500

    Updated version

--- src/var/version.py
+++ src/var/version.py
@@ -1,6 +1,6 @@
-version = "v1.05"
-versionDate = "7 March, 2024"
+version = "v1.06"
+versionDate = "8 March, 2024"
 author = "Michael Braine"
 changes = [
-    "- added dof=2 as option",
+    "- NIST-distributed CSVs from FEL calibrations can now be imported",
 ]
